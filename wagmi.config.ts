11d47a92f92ae9e60f92ec8bb9ac433d086a988e
Hayatti <gurujowa@gmail.com>
2023-12-24 08:52:17 +0000

    Added poolinitializer

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -4,6 +4,7 @@ import { etherscan } from '@wagmi/cli/plugins'
 import IUniswapV3PoolABI from '@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json' assert { type: "json" };
 import QuoterV2 from '@uniswap/v3-periphery/artifacts/contracts/lens/QuoterV2.sol/QuoterV2.json' assert { type: "json" };
 import Quoter from '@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json' assert { type: "json" };
+import PoolInitializer from '@uniswap/v3-periphery/artifacts/contracts/interfaces/IPoolInitializer.sol/IPoolInitializer.json' assert { type: "json" };
 import SwapRouter from '@uniswap/v3-periphery/artifacts/contracts/SwapRouter.sol/SwapRouter.json' assert { type: "json" };
 import NonfungiblePositionManager from '@uniswap/v3-periphery/artifacts/contracts/NonfungiblePositionManager.sol/NonfungiblePositionManager.json' assert { type: "json" };
 
@@ -13,6 +14,10 @@ export default defineConfig({
     {
       abi: IUniswapV3PoolABI.abi,
       name: 'IUniswapV3Pool',
+    },
+    {
+      abi: PoolInitializer.abi,
+      name: 'PoolInitializer',
     },
     {
       abi: Quoter.abi,
